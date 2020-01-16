b34fe27c2af5346d206403da3a6571252c46c188
zanyxdev <zanyxdev@gmail.com>
2020-01-16 15:43:14 +0300

    dump stup for run emulation

--- microsha/main.cpp
+++ microsha/main.cpp
@@ -1,10 +1,28 @@
-#include <QCoreApplication>
+#include <QApplication>
 #include <QDebug>
+#include <QDateTime>
 #include "../app_version.h"
+#include "runner.h"
+#include "i8080.h"
+
+#define FREQ 2100000
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QString version = QString("%1.%2.%3 build time[%4]")
+            .arg(VER_MAJOR)
+            .arg(GIT_VERSION)
+            .arg(APP_COMMIT_COUNT)
+            .arg(APP_TIME);
 
-    return a.exec();
+    qDebug() << "APP_TIME:"<<APP_TIME<< " version:" <<version;
+
+    //Q_INIT_RESOURCE(microsha_res);
+    QApplication app(argc, argv);
+    app.setApplicationName(QString("Microsha QT5 emulator. ver:%1").arg(version));
+
+    i8080 *cpu = new i8080(&app);
+    Runner *runner = new Runner(&app);
+    runner->setCPU(cpu,FREQ);
+    return app.exec();
 }
